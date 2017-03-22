b8ad5e4ef3dbe0e080dff1fbe61b2fda1176fc31
Tanel Eiskop <tan1983@hot.ee>
2017-03-22 14:22:26 +0100

    added RBAC to customer-discount index

--- backend/views/customer-discount/index.php
+++ backend/views/customer-discount/index.php
@@ -3,6 +3,8 @@
 use yii\helpers\Html;
 use yii\grid\GridView;
 use yii\widgets\Pjax;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Url;
 /* @var $this yii\web\View */
 /* @var $searchModel backend\models\CustomerDiscountSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
@@ -65,9 +67,50 @@ $this->params['breadcrumbs'][] = $this->title;
             // 'approved_by',
             // 'active',
 
-            [
+[  
                 'class' => 'yii\grid\ActionColumn',
-                'contentOptions'=>['style'=>'width:70px;'],
+                'contentOptions' => ['style' => 'width:50px;'],
+                'header'=>'',
+                'template' => '{view} {update}',
+                'buttons' => 
+                [
+
+                    //view button
+                    'view' => function ($url, $model) {
+                        return Html::a('<span class="glyphicon glyphicon-eye-open"></span>', $url, [
+                                    'title' => Yii::t('app', 'View'),                              
+                        ]);
+                    },
+                    'update' => function ($url, $model) {
+                        if (Yii::$app->user->can('update-customerdiscount') OR Yii::$app->user->can('change-so')) 
+                        {
+                            return Html::a('<span class="glyphicon glyphicon-pencil"></span>', $url, [
+                                        'title' => Yii::t('app', 'Update'),                              
+                            ]);
+                        }
+                    },
+                    'delete' => function ($url, $model) {
+                        if (Yii::$app->user->can('delete-customerdiscount')) 
+                        {
+                            return Html::a('<span class="glyphicon glyphicon-trash"></span>', $url, [
+                                        'title' => Yii::t('app', 'Delete'),
+                                        'data-confirm' => 'Are you sure you want to delete this item?',
+                                        'data-method' => 'post'
+                            ]);
+                        }
+                    },                                           
+                ],
+                'urlCreator' => function ($action, $model, $key, $index) {
+                    if ($action === 'view') {
+                        return Url::to(['customer-discount/view', 'id'=>$model->id]);
+                    }
+                    if ($action === 'update') {
+                        return Url::to(['customer-discount//update', 'id'=>$model->id]);
+                    }
+                    if ($action === 'delete') {
+                        return Url::to(['customer-discount//delete', 'id'=>$model->id]);
+                    }                    
+                }
             ],
         ],
     ]); ?>
